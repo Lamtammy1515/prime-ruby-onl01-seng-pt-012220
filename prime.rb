62c2e40702482e77f00c1089a61ea968393d2961
Tammy Lam <lamtammy1515@gmail.com>
2020-01-31 01:06:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,10 @@
 # Add  code here!
 def prime?(number)
-  if number > 2
-   return false 
-  else 
-    return true
 (2..number-1).none? do |num|
   number % num == 0
-  
 end
-end 
+  
+if number > 2
+   return false 
+  end 
 end 
